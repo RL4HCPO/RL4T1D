962359a91bbfe76d990cdd8055c958e5da309da7
DilshanRakshitha <dilshanrakshitha2000@gmail.com>
2025-03-13 19:05:56 +0000

    pcpo updated

--- agents/algorithm/pcpo.py
+++ agents/algorithm/pcpo.py
@@ -110,99 +110,99 @@ class PCPO(Agent):
         continue_pi_training, buffer_len = True, self.rollout_buffer['len']
         constraint = self.rollout_buffer['constraint']
         policy_grad_ = 0
-        for i in range(self.train_pi_iters):
-            start_idx, n_batch = 0, 0
-            while start_idx < buffer_len:
-                n_batch += 1
-                end_idx = min(start_idx + self.batch_size, buffer_len)
+        # for i in range(self.train_pi_iters):
+            # start_idx, n_batch = 0, 0
+            # while start_idx < buffer_len:
+        # n_batch += 1
+        # end_idx = min(start_idx + self.batch_size, buffer_len)
 
-                states_batch = self.rollout_buffer['states'][start_idx:end_idx, :, :]
-                actions_batch = self.rollout_buffer['action'][start_idx:end_idx, :]
-                logprobs_batch = self.rollout_buffer['log_prob_action'][start_idx:end_idx, :]
-                advantages_batch = self.rollout_buffer['advantage'][start_idx:end_idx]
-                advantages_batch = (advantages_batch - advantages_batch.mean()) / (advantages_batch.std() + 1e-5)
-                cost_advantages_batch = self.rollout_buffer['cost_advantage'][start_idx:end_idx]
-                cost_advantages_batch = (cost_advantages_batch - cost_advantages_batch.mean()) / (cost_advantages_batch.std() + 1e-5)
+        states_batch = self.rollout_buffer['states'][:, :, :]
+        actions_batch = self.rollout_buffer['action'][:, :]
+        logprobs_batch = self.rollout_buffer['log_prob_action'][:, :]
+        advantages_batch = self.rollout_buffer['advantage'][:]
+        advantages_batch = (advantages_batch - advantages_batch.mean()) / (advantages_batch.std() + 1e-5)
+        cost_advantages_batch = self.rollout_buffer['cost_advantage'][:]
+        cost_advantages_batch = (cost_advantages_batch - cost_advantages_batch.mean()) / (cost_advantages_batch.std() + 1e-5)
 
-                logprobs_prediction, dist_entropy = self.policy.evaluate_actor(states_batch, actions_batch)
-                ratios = torch.exp(logprobs_prediction - logprobs_batch)
-                ratios = ratios.squeeze()
-                r_theta = ratios * advantages_batch
-                # + self.policy.Actor.PolicyModule.penalty * 0.00001
-                # policy_loss = -r_theta.mean() - self.entropy_coef * dist_entropy.mean() 
-                policy_loss = -r_theta.mean()
+        logprobs_prediction, dist_entropy = self.policy.evaluate_actor(states_batch, actions_batch)
+        ratios = torch.exp(logprobs_prediction - logprobs_batch)
+        ratios = ratios.squeeze()
+        r_theta = ratios * advantages_batch
+        # + self.policy.Actor.PolicyModule.penalty * 0.00001
+        # policy_loss = -r_theta.mean() - self.entropy_coef * dist_entropy.mean() 
+        policy_loss = -r_theta.mean()
 
-                # early stop: approx kl calculation
-                log_ratio = logprobs_prediction - logprobs_batch
-                approx_kl = torch.mean((torch.exp(log_ratio) - 1) - log_ratio).detach().cpu().numpy()
-                if approx_kl > 1.5 * self.target_kl:
-                    if self.args.verbose:
-                        print('Early stop => Epoch {}, Batch {}, Approximate KL: {}.'.format(i, n_batch, approx_kl))
-                    continue_pi_training = False
-                    break
+        # early stop: approx kl calculation
+        log_ratio = logprobs_prediction - logprobs_batch
+        approx_kl = torch.mean((torch.exp(log_ratio) - 1) - log_ratio).detach().cpu().numpy()
+        # if approx_kl > 1.5 * self.target_kl:
+        #     if self.args.verbose:
+        #         print('Early stop => Epoch {}, Batch {}, Approximate KL: {}.'.format(i, n_batch, approx_kl))
+        #     continue_pi_training = False
+        #     break
 
-                if torch.isnan(policy_loss):  # for debugging only!
-                    print('policy loss: {}'.format(policy_loss))
-                    exit()
+        if torch.isnan(policy_loss):  # for debugging only!
+            print('policy loss: {}'.format(policy_loss))
+            exit()
 
-                temp_loss_log += policy_loss.detach()
-                policy_grad = torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), self.grad_clip)
-                # policy_grad = torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), max_norm=1.0)
-                policy_grad_ += policy_grad # used to returing mean_pi_gradient at the end
-                grads = torch.autograd.grad(policy_loss, self.policy.Actor.parameters(), retain_graph=True)
-                loss_grad = torch.cat([grad.view(-1) for grad in grads])
-                # implement gradient normalizing if want here
+        temp_loss_log += policy_loss.detach()
+        policy_grad = torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), self.grad_clip)
+        # policy_grad = torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), max_norm=1.0)
+        policy_grad_ += policy_grad # used to returing mean_pi_gradient at the end
+        grads = torch.autograd.grad(policy_loss, self.policy.Actor.parameters(), retain_graph=True)
+        loss_grad = torch.cat([grad.view(-1) for grad in grads])
+        # implement gradient normalizing if want here
 
-                # finding the step direction / add direct hessian finding function here later. get the parameter from args
-                Fvp = Fvp_fim
-                stepdir = conjugate_gradients(Fvp, -loss_grad, 10) #point which minimizes the loss
-                # if gradient normalizing, normalize the step dir here
+        # finding the step direction / add direct hessian finding function here later. get the parameter from args
+        Fvp = Fvp_fim
+        stepdir = conjugate_gradients(Fvp, -loss_grad, 10) #point which minimizes the loss
+        # if gradient normalizing, normalize the step dir here
 
-                # findign cost loss
-                c_theta = ratios * cost_advantages_batch
-                # + self.policy.Actor.PolicyModule.penalty * 0.00001
-                """cost_loss = -c_theta.mean() - self.entropy_coef * dist_entropy.mean()
-                initially had this, no need for the- sign in cost loss, as we are minimizing the cost function"""
-                cost_loss = c_theta.mean()
+        # findign cost loss
+        c_theta = ratios * cost_advantages_batch
+        # + self.policy.Actor.PolicyModule.penalty * 0.00001
+        """cost_loss = -c_theta.mean() - self.entropy_coef * dist_entropy.mean()
+        initially had this, no need for the- sign in cost loss, as we are minimizing the cost function"""
+        cost_loss = c_theta.mean()
 
-                #finding the cost step direction
-                cost_grads = torch.autograd.grad(cost_loss, self.policy.Actor.parameters())
-                cost_loss_grad = torch.cat([grad.view(-1) for grad in cost_grads]) 
-                # cost_loss_grad = cost_loss_grad / torch.norm(cost_loss_grad)
-                cost_stepdir = conjugate_gradients(Fvp, -cost_loss_grad, 10) #point which minimizes the cost loss
+        #finding the cost step direction
+        cost_grads = torch.autograd.grad(cost_loss, self.policy.Actor.parameters())
+        cost_loss_grad = torch.cat([grad.view(-1) for grad in cost_grads]) 
+        # cost_loss_grad = cost_loss_grad / torch.norm(cost_loss_grad)
+        cost_stepdir = conjugate_gradients(Fvp, -cost_loss_grad, 10) #point which minimizes the cost loss
 
-                # Define q, r, s
-                """p is feels to be worng -> p = cost_loss_grad.dot(stepdir)
-                    but since p is not being used, won't change it. """
-                p = cost_loss_grad.dot(stepdir) #a^T.H^-1.g
-                q = -loss_grad.dot(stepdir) #g^T.H^-1.g
-                r = loss_grad.dot(cost_stepdir) #g^T.H^-1.a
-                s = -cost_loss_grad.dot(cost_stepdir) #a^T.H^-1.a 
+        # Define q, r, s
+        """p is feels to be worng -> p = cost_loss_grad.dot(stepdir)
+            but since p is not being used, won't change it. """
+        p = cost_loss_grad.dot(stepdir) #a^T.H^-1.g
+        q = -loss_grad.dot(stepdir) #g^T.H^-1.g
+        r = loss_grad.dot(cost_stepdir) #g^T.H^-1.a
+        s = -cost_loss_grad.dot(cost_stepdir) #a^T.H^-1.a 
 
-                print('s =', s, 'r =', r, 'q =', q, 'p =', p)
-                epsilon = 1e-8
-                policy_update = torch.sqrt(2*self.max_kl/(q + epsilon))*stepdir
-                project_val = ((torch.sqrt(2*self.max_kl/(q + epsilon)) * p) + (constraint - self.d_k)) / (s + epsilon)
-                print('(torch.sqrt(2*self.max_kl/(q + epsilon)) * p) = ',(torch.sqrt(2*self.max_kl/(q + epsilon)) * p))
-                print('(constraint - self.d_k)) / (s + epsilon) = ', ((constraint - self.d_k)) / (s + epsilon))
-                print('self.d_k = ', self.d_k)
-                print('constraint - self.d_k = ', constraint - self.d_k)
-                print("project_val = ", project_val.item())
-                projection = (max(0.0, project_val.item())) * cost_stepdir
-                opt_step = policy_update + projection
-                
-                # trying without line search
-                print("constraint =", constraint)
-                prev_params = get_flat_params_from(self.policy.Actor)
-                new_params = prev_params + opt_step
-                set_flat_params_to(self.policy.Actor, new_params)
+        print('s =', s, 'r =', r, 'q =', q, 'p =', p)
+        epsilon = 1e-8
+        policy_update = torch.sqrt(2*self.max_kl/(q + epsilon))*stepdir
+        project_val = ((torch.sqrt(2*self.max_kl/(q + epsilon)) * p) + (constraint - self.d_k)) / (s + epsilon)
+        print('(torch.sqrt(2*self.max_kl/(q + epsilon)) * p) = ',(torch.sqrt(2*self.max_kl/(q + epsilon)) * p))
+        print('(constraint - self.d_k)) / (s + epsilon) = ', ((constraint - self.d_k)) / (s + epsilon))
+        print('self.d_k = ', self.d_k)
+        print('constraint - self.d_k = ', constraint - self.d_k)
+        print("project_val = ", project_val.item())
+        projection = (max(0.0, project_val.item())) * cost_stepdir
+        opt_step = policy_update + projection
+        
+        # trying without line search
+        print("constraint =", constraint)
+        prev_params = get_flat_params_from(self.policy.Actor)
+        new_params = prev_params + opt_step
+        set_flat_params_to(self.policy.Actor, new_params)
 
-                #######
-                pol_count += 1
-                start_idx += self.batch_size
+        #######
+        pol_count += 1
+        # start_idx += self.batch_size
 
-            if not continue_pi_training:
-                break
+            # if not continue_pi_training:
+            #     break
         mean_pi_grad = policy_grad_ / pol_count if pol_count != 0 else 0
         print('The policy loss is: {}'.format(temp_loss_log))
         return mean_pi_grad, temp_loss_log
